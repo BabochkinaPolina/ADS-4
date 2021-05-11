90252f8b994b12d843c49585f198ff3104f05cf6
BabochkinaPolina <79091702+BabochkinaPolina@users.noreply.github.com>
2021-05-11 12:22:56 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -2,65 +2,68 @@
 #ifndef INCLUDE_TPQUEUE_H_
 #define INCLUDE_TPQUEUE_H_
 #include <cassert>
-
 template<typename T>
 class TPQueue {
  private:
-    T* arr;
+    T *arr;
     int size;
-    int begin, end;
+    int begin,
+        end;
     int count;
  public:
-    TPQueue();
+    explicit TPQueue(int size = 100);
     ~TPQueue();
-    void push(const T&);
+    void push(const T &);
     T pop();
     T get() const;
     bool isEmpty() const;
     bool isFull() const;
 };
 template<typename T>
-TPQueue<T>::TPQueue() : size(100),
-begin(0), end(0), count(0) {
-  arr = new T[size + 1];
+TPQueue<T>::TPQueue(int sizePQueue) :
+size(sizePQueue), begin(0), end(0), count(0) {
+    arr = new T[size + 1];
 }
 template<typename T>
 TPQueue<T>::~TPQueue() {
-  delete [] arr;
+    delete [] arr;
 }
 template<typename T>
 void TPQueue<T>::push(const T & item) {
-  assert(count < size);
-  end++;
-  arr[end].ch = item.ch;
-  arr[end].prior = item.prior;
-  count++;
-  for (int i = end; i > begin; i--) {
-    if (arr[i].prior > arr[i - 1].prior) {
-      char k = arr[i].ch;
-      arr[i].ch = arr[i - 1].ch;
-      arr[i - 1].ch = k;
-      int k2 = arr[i].prior;
-      arr[i].prior = arr[i - 1].prior;
-      arr[i - 1].prior = k2;
+    assert(count < size);
+    if (end != 0) {
+      for (int i1 = end - 1; i1 > -1; i1--) {
+        if (item.prior > arr[i1].prior) {
+           arr[i1+1] = arr[i1];
+            if (i1 == 0) arr[i1] = item;
+        } else {
+          if (item.prior <= arr[i1].prior) {
+              arr[i1 + 1] = item;
+          }
+        }
+      }
+    }  else {
+      arr[end] = item;
     }
-  }
-  if (end > size)
-    end -= size + 1;
+    end++;
+    count++;
 }
 template<typename T>
 T TPQueue<T>::pop() {
   assert(count > 0);
   T item = arr[begin++];
   count--;
-  if (begin > size)
-      begin -= size + 1;
-  return item;
+
+if (begin > size) {
+begin -= size + 1;
 }
+return item;
+}
+
 template<typename T>
 T TPQueue<T>::get() const {
-  assert(count > 0);
-  return arr[begin];
+assert(count > 0);
+return arr[begin];
 }
 template<typename T>
 bool TPQueue<T>::isEmpty() const {
@@ -68,10 +71,10 @@ bool TPQueue<T>::isEmpty() const {
 }
 template<typename T>
 bool TPQueue<T>::isFull() const {
-  return count == size;
+return count == size;
 }
 struct SYM {
-  char ch;
-  int  prior;
+char ch;
+int  prior;
 };
 #endif // INCLUDE_TPQUEUE_H_
